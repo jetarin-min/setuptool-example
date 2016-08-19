dc45a02db268000012bea98a3885ac77c00e846e
jetarin-min <jetarin.min@gmail.com>
2016-08-19 12:21:54 +0700

    first commit

---
+++ modules/core_module/core_module/configs/main_config.py
@@ -0,0 +1,2 @@
+class test:
+    var1 = 'ABC'
---
+++ modules/core_module/setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(name="core_module",
+    version = "1.0.0",
+    description = "Core Module Framework",
+)
